0c32a7d87b0d20bdcf0ea8b1f0f3360a5f3cbbca
madhuriMedaboina <madhurimedaboina@gmail.com>
2023-08-13 09:50:04 +0000

    export coding practice solution

---
+++ src/components/Products/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+
+import Header from '../Header'
+import './index.css'
+
+const Product = () => (
+  <>
+    <Header />
+    <div className="product-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="products-img"
+      />
+    </div>
+  </>
+)
+
+export default Product
